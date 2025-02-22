e4dd93bc6a6380ead0fa3802ec597c66f77a5139
Rasmus Aaen <ra@emd.dk>
2025-02-22 14:34:10 +0100

    Update config

--- include/config.h
+++ include/config.h
@@ -26,7 +26,7 @@
 
 // #define INCLUDE_CLASSIC_I2C_SUPPORT  // Uncomment to include classic Serial to I2C support (excludes body master built)
 #define INCLUDE_I2C_SLAVE       // Uncommend for new I2C slave mode
-// #define INCLUDE_HOLO_RGB     // Uncomment for NeoPixel-Holo (excludes body master built)
+#define INCLUDE_HOLO_RGB     // Uncomment for NeoPixel-Holo (excludes body master built)
 // #define SEPARATE_DOMELIFT    // uncomment, if you want to use (Master) AUX as separate Dome Lift Serial
 
 #ifdef SEPARATE_DOMELIFT
